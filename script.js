e2f1f29737fa735d6494bc2db3d574b91767a3e9
enesm <e.elturky@elham.sa>
2024-05-29 15:44:29 +0300

    formate

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper-container", {
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-  document.querySelectorAll(".start-now").forEach(function (button) {
-    button.addEventListener("click", function (event) {
+    const swiper = new Swiper(".swiper-container", {
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
-  });
 
-  const togglerButton = document.querySelector("button")
-  const mobileMenu = document.querySelector(".navbar-collapse")
-  togglerButton.addEventListener("click",
-  ()=>{
-    if(mobileMenu.classList.contains("show")){
-        console.log(mobileMenu.classList);
-        mobileMenu.classList.remove("show")
-    }else{
-        mobileMenu.classList.add("show");
-    }
-    
-  }
+    document.querySelectorAll(".start-now").forEach(function (button) {
+        button.addEventListener("click", function (event) {
+        });
+    });
 
-  )
+    const togglerButton = document.querySelector("button")
+    const mobileMenu = document.querySelector(".navbar-collapse")
+    togglerButton.addEventListener("click",
+        () => {
+            if (mobileMenu.classList.contains("show")) {
+                console.log(mobileMenu.classList);
+                mobileMenu.classList.remove("show")
+            } else {
+                mobileMenu.classList.add("show");
+            }
+
+        }
+    )
 });
 
 
